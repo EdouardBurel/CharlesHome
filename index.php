b0feb9021877bc18db164ff37bced18f2a4179b8
Edouard Burel <bureledo@gmail.com>
2023-06-07 15:42:16 +0200

    login test

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+<?php
+    ini_set('display_errors', 'off');
+    session_start();
+    $pdo = new PDO('mysql:dbname=edouardburel_charleshome;host=mysql-edouardburel.alwaysdata.net', '302132_chome', 'Charleshome1');
+    $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    if(!isset($_SESSION['user_id'])) {
+        header('location: login.php');
+    }
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -17,6 +30,17 @@
     <header>
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
             <div class="container-fluid">
+            <?php
+                $id = (int)$_SESSION['user_id'];
+                $query = "SELECT * FROM user WHERE id = ?";
+                $res = $pdo->prepare($query);
+                $res->execute([$id]);
+
+                $user = $res->fetch(PDO::FETCH_ASSOC);
+
+                $reservationName = (string)$user['firstName'].' '.(string)$user['lastName'];
+
+                echo <<<HTML
               <img src="image/logo_charles-home.png" alt="CHARLES HOME">
               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -32,12 +56,17 @@
                   <li class="nav-item">
                     <a class="nav-link" href="#" style="color:red">Emergency</a>
                   </li>
+                    <li class="nav-item">
+                        <a href="logout.php" class="nav-link">Se déconnecter</a>
+                    </li>
                 </ul>
                 <span class="helloText navbar-text">
-                    Hello John Doe
+                    Hello $reservationName !
                 </span>
               </div>
             </div>
+            HTML;
+            ?>
         </nav>
     </header>
     <main>
---
+++ login.php
@@ -0,0 +1,97 @@
+<?php
+session_start();
+
+$pdo = new PDO('mysql:dbname=edouardburel_charleshome;host=mysql-edouardburel.alwaysdata.net', '302132_chome', 'Charleshome1');
+$pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+if(isset($_POST['submit'])){
+    $email= $_POST['email'];
+    $password = $_POST['password'];
+
+    $query = $pdo->prepare("SELECT * FROM user WHERE email = :email");
+    $query->bindValue('email', $email);
+    $query->execute();
+    $res = $query->fetch(PDO::FETCH_ASSOC);
+
+    if ($res) {
+
+       if($res['role'] == 'admin'){
+   
+          $_SESSION['admin_id'] = $res['id'];
+          header('location:/admin/admin.php');
+ 
+       }elseif($res['role'] == 'user'){
+ 
+          $_SESSION['user_id'] = $res['id'];
+          header('location:index.php');
+
+       }
+    }
+ }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+    <style>
+        body {
+            background-color: black;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            min-height: 100vh;
+            font-family: Arial, sans-serif;
+        }
+
+        .login-container {
+            background-color: #fff;
+            padding: 30px;
+            border-radius: 5px;
+            text-align: center;
+        }
+
+        .login-container img {
+            width: 150px;
+            margin-bottom: 20px;
+        }
+
+        .login-container input {
+            width: 100%;
+            padding: 10px;
+            margin-bottom: 10px;
+            border-radius: 3px;
+            border: 1px solid #ccc;
+        }
+
+        .login-container button {
+            width: 100%;
+            padding: 10px;
+            background-color: #ff8f00;
+            color: #fff;
+            border: none;
+            border-radius: 3px;
+            cursor: pointer;
+        }
+
+        .login-container button:hover {
+            opacity: 0.7;
+        }
+    </style>
+</head>
+<body>
+    <div class="login-container">
+        <img src="image/logo_charles-home.png" alt="Logo">
+        <form method="POST" enctype="multipart/form-data">
+            <input name="email" type="email" placeholder="Email" required><br>
+            <input name="password" type="password" placeholder="Password" required><br>
+            <button name="submit" type="submit">Login</button>
+        </form>
+    </div>
+</body>
+</html>

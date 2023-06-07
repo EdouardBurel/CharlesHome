b185f67ae5fdcfff7f5d7f6b5a6cb85d7765e241
Edouard Burel <bureledo@gmail.com>
2023-06-07 11:33:06 +0200

    add form extension request

---
+++ script.js
@@ -0,0 +1,20 @@
+function showExtensionForm() {
+    document.getElementById("overlay").style.display = "block";
+}
+
+function closeExtensionForm() {
+    document.getElementById("overlay").style.display = "none";
+}
+
+
+/*
+function showExtensionForm() {
+    var overlay = document.getElementById("overlay");
+    overlay.style.display = "flex";
+}
+
+function closeExtensionForm() {
+    var overlay = document.getElementById("overlay");
+    overlay.style.display = "none";
+}
+*/
